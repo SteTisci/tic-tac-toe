3db94ac63b7c1d39216663661c5633939c13e6cd
SteTisci <ste.tisci@gmail.com>
2024-08-25 07:16:07 +0200

    aggiunta gameUI

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const gameBoard = (function () {
     2: [null, null, null],
   };
 
-  const clearBoard = () => {
+  const clear = () => {
     for (let i = 0; i < 3; i++) {
       board[i].splice(0, 3, null, null, null);
     }
@@ -22,76 +22,115 @@ const gameBoard = (function () {
   // The index contains a 2 digit string representing:
   // 1 digit = row , 2 digit = column
   // example: index = "10" --> board[1][0]
-  const updateBoard = (sign, index) => {
+  const update = (sign, index) => {
     const [row, col] = index.split("");
     if (board[row][col] === null) {
       board[row].splice(col, 1, sign);
     }
   };
 
-  const boardStatus = () => board;
+  const status = () => board;
 
-  return { clearBoard, updateBoard, boardStatus };
+  return { clear, update, status };
 })();
 
 // The gameController factory represent the state of the game
 // Look for possible tris and determine the end and winner of a game
-// Inherits the board state from gameBoard
 const gameController = (function () {
-  const { boardStatus } = gameBoard;
+  const board = gameBoard;
   const players = [player("player 1", "X"), player("player 2", "O")];
-  let winnerSign = "";
-  let winner = "";
-  let gameEnded = false;
+  let currentPlayer = players[0];
+  let gameEnd = false;
+
+  const playRound = (sign, index) => {
+    board.update(sign, index);
+    checkWinner();
+    currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+  };
 
   // Check every cell of a determined tris of the board
   // Returns true if all 3 are equal and !null or false if not
-  function checkCombination(cells) {
+  const checkCombination = (cells) => {
     return cells.every((cell) => cell !== null && cell === cells[0]);
-  }
+  };
 
   // Finds if a tris has been made in all the possible board combinations
-  function findWinningCombinations() {
-    const currentBoard = boardStatus();
+  // Choose the winner based on the tris sign
+  const checkWinner = () => {
+    const currentBoard = board.status();
+
+    // Main and Secondary diagonal
+    let diagonal1 = [currentBoard[0][0], currentBoard[1][1], currentBoard[2][2]];
+    let diagonal2 = [currentBoard[0][2], currentBoard[1][1], currentBoard[2][0]];
+    if (checkCombination(diagonal1)) {
+      gameEnd = true;
+    }
+    if (checkCombination(diagonal2)) {
+      gameEnd = true;
+    }
 
     // Rows and Columns
     for (let i = 0; i < 3; i++) {
       let row = currentBoard[i];
-      let column = [currentBoard[0][i], currentBoard[1][i], currentBoard[2][i]];
-
+      let col = [currentBoard[0][i], currentBoard[1][i], currentBoard[2][i]];
       if (checkCombination(row)) {
-        winnerSign = row[0];
-        break; // Stops the cicle if a winnerSign combination is found
+        gameEnd = true;
       }
-      if (checkCombination(column)) {
-        winnerSign = column[0];
-        break;
+      if (checkCombination(col)) {
+        gameEnd = true;
       }
     }
-    // Main and Secondary diagonal
-    let diagonal1 = [currentBoard[0][0], currentBoard[1][1], currentBoard[2][2]];
-    let diagonal2 = [currentBoard[0][2], currentBoard[1][1], currentBoard[2][0]];
-
-    if (checkCombination(diagonal1)) {
-      winnerSign = diagonal1[0];
-    }
-    if (checkCombination(diagonal2)) {
-      winnerSign = diagonal2[0];
-    }
-  }
-
-  // FIXME: da sistemare la logica di fine gioco e decisione vincitore
-  const gameOver = () => {
-    gameEnded = winner ? true : false;
   };
-  const isGameOver = () => gameEnded;
 
-  const setGameWinner = () => {
-    if (winnerSign) {
-      winner = winnerSign === "X" ? players[0] : players[1];
-    }
+  const isGameOver = () => gameEnd;
+  const getCurrentPlayer = () => currentPlayer;
+
+  const newGame = () => {
+    board.clear();
+    gameEnd = false;
+    currentPlayer = players[0];
   };
-  const getGameWinner = () => winner;
 
-  return { findWinningCombinations, gameOver, isGameOver, setGameWinner, getGameWinner };
+  return { playRound, getCurrentPlayer, isGameOver, newGame };
 })();
+
+const gameUI = (function (doc) {
+  const controller = gameController;
+  const boardContainer = doc.querySelector(".board-container");
+  const winnerParagraph = doc.querySelector(".winner");
+  const index = ["00", "01", "02", "10", "11", "12", "20", "21", "22"];
+
+  const createBoard = () => {
+    for (let i = 0; i < 9; i++) {
+      const cell = doc.createElement("div");
+      cell.setAttribute("class", `cell ${index[i]}`);
+      boardContainer.appendChild(cell);
+    }
+  };
+
+  const playGame = () => {
+    createBoard();
+
+    boardContainer.addEventListener("click", (event) => {
+      const cell = event.target.closest(".cell");
+      const cellIndex = cell.classList[1];
+      const currentPlayer = controller.getCurrentPlayer();
+
+      if (!cell.textContent) {
+        cell.textContent = currentPlayer.sign;
+        controller.playRound(currentPlayer.sign, cellIndex);
+        console.table(gameBoard.status());
+      }
+      if (controller.isGameOver()) {
+        winnerParagraph.textContent = currentPlayer.name;
+        //TODO: aggiungere messaggio vittoria
+      }
+    });
+  };
+
+  return { playGame };
+})(document);
+
+const interface = gameUI;
+
+interface.playGame();
